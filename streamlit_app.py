256aa4f58bc8055e8ee359e9658d15752e339385
armussah-jacobs <134394128+armussah-jacobs@users.noreply.github.com>
2023-06-07 12:11:15 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 from streamlit_option_menu import option_menu
-from apps import home, heatmap, upload  # import your app modules here
+from apps import home, heatmap, upload, vector  # import your app modules here
 
 st.set_page_config(page_title="Streamlit Geospatial", layout="wide")
 
@@ -9,7 +9,7 @@ st.set_page_config(page_title="Streamlit Geospatial", layout="wide")
 
 apps = [
     {"func": home.app, "title": "Home", "icon": "house"},
-    {"func": upload.app, "title": "Upload", "icon": "cloud-upload"},
+    {"func": vector.app, "title": "Upload", "icon": "cloud-upload"},
 ]
 
 titles = [app["title"] for app in apps]
